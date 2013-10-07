131fcf79cf4be5bcbc1fdc24a6cf21a7ef21bab4
MarcinRa <marcin.radon@recoded.co>
2013-10-07 15:16:41 +0200

    password reminder added

--- zdecydujmyrazem/urls.py
+++ zdecydujmyrazem/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import RedirectView
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 admin.autodiscover()
 
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
 
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'zr/index.html'}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
+    url(r'^accounts/', include('registration.urls')),
     url(r'^social_auth/', include('social_auth.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
