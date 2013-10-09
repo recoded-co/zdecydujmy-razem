09a8906eb4c9cd986caa7bc69985e888496b9307
Dariusz Walczak <walczak.darek@gmail.com>
2013-10-09 15:30:44 +0200

    login bugfix

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zdecydujmyrazem.settings")
     sys.path.insert(0, './dep')
+    sys.path.insert(1, '/Library/Python/2.7/site-packages/')
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
