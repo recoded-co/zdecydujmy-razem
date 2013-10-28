0f69681dfe8d675b22483e39ee95938a9839ace4
MarcinRa <marcin.radon@recoded.co>
2013-10-28 15:51:39 +0100

    rest permissions settings updated

--- zdecydujmyrazem/base.py
+++ zdecydujmyrazem/base.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
 }
