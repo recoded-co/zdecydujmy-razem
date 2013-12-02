348074ab88ec92f864e805c5e9596d97781b558d
MarcinRa <marcin.radon@recoded.co>
2013-12-02 13:37:24 +0100

    file download strategy changed

--- filemanager/urls.py
+++ filemanager/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, url, include
 from filemanager.views import FileManager, angular_post
+from zdecydujmyrazem import settings
 
 urlpatterns = patterns('',
-    url(r'^/(?P<file_id>.+)$',FileManager.as_view()),
-    url(r'^$', FileManager.as_view(), name="fm"),
+    #url(r'^(?P<file_id>.+)$',FileManager.as_view()),
+    url(r'^download/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
     url(r'^angular_post$','filemanager.views.angular_post' ),
+    url(r'^$', FileManager.as_view(), name="fm"),
+
 )
--- zr/static/angular/directives.js
+++ zr/static/angular/directives.js
@@ -8,11 +8,17 @@ zdApp.directive('fileChange',['uploadService', function (uploadService) {
         element.bind('change', function (event) {
             var files = event.target.files;
             for (var i = 0, length = files.length; i < length; i++) {
-                uploadService.send(files[i],attributes.value, function(data){
-                    var json_data = JSON.parse(data);
-                    $scope.$parent.post.filep.push({name:json_data.name,id:json_data.id});
-                    $scope.$apply();
-                });
+                if(files[i].size < 5242880)
+                    uploadService.send(files[i],$scope.$parent.post.id, function(data){
+                        var json_data = JSON.parse(data);
+                        $scope.$parent.post.filep.push({name:json_data.name,
+                            id:json_data.id,
+                            file:json_data.file});
+                        $scope.$apply();
+                    });
+                else{
+                    alert('plik nie powinien przekraczać 5Mb');
+                }
             }
         });
     };
@@ -46,11 +52,17 @@ zdApp.directive('dropbox',['uploadService', function (uploadService) {
             event.preventDefault();
             var files = event.dataTransfer.files;
             for (var i = 0, length = files.length; i < length; i++) {
-                uploadService.send(files[i],$scope.$parent.post.id, function(data){
-                    var json_data = JSON.parse(data);
-                    $scope.$parent.post.filep.push({name:json_data.name,id:json_data.id});
-                    $scope.$apply();
-                });
+                if(files[i].size < 5242880)
+                    uploadService.send(files[i],$scope.$parent.post.id, function(data){
+                        var json_data = JSON.parse(data);
+                        $scope.$parent.post.filep.push({name:json_data.name,
+                            id:json_data.id,
+                            file:json_data.file});
+                        $scope.$apply();
+                    });
+                else{
+                    alert('plik nie powinien przekraczać 5Mb');
+                }
             }
         };
     };
