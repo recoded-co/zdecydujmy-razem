1553608bb4600b480f60770bd22ade87d5f6aa72
MarcinRa <marcin.radon@recoded.co>
2014-01-02 13:26:51 +0100

    starting work with socia auth pipelines

--- zdecydujmyrazem/base.py
+++ zdecydujmyrazem/base.py
@@ -122,6 +122,16 @@ LOGGING = {
     }
 }
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_auth.backends.pipeline.social.social_auth_user',
+    'social_auth.backends.pipeline.associate.associate_by_email',
+    'social_auth.backends.pipeline.user.get_username',
+    'social_auth.backends.pipeline.user.create_user',
+    'social_auth.backends.pipeline.social.associate_user',
+    'social_auth.backends.pipeline.user.update_user_details',
+    #'auth_pipelines.pipelines.get_user_avatar',
+)
+
 AUTHENTICATION_BACKENDS = (
     'social_auth.backends.facebook.FacebookBackend',
     'social_auth.backends.google.GoogleOAuth2Backend',
