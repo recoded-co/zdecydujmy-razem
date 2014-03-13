465982c1d4d3f6d5a069a73481596f156442ad44
Darek Walczak <walczak.darek@gmail.com>
2014-03-13 13:57:29 +0100

    filter cancelation

--- zr/static/angular/controllers.js
+++ zr/static/angular/controllers.js
@@ -101,11 +101,15 @@ zdControllers.controller('apiList', ['$scope','$http','$cookies','$rootScope', '
         }
     };
     $scope.showAll = function(){
+        $scope.filterGeoData=[];
+        $scope.searchdate.date='';
+        $scope.showallposts = true;
+        /*
           if($scope.showallposts == false){
               $scope.showallposts = true;
           }else{
               $scope.showallposts = false;
-          }
+          }*/
       };
     $scope.addVertical = function(data) {
         if (data.nodes !== undefined)
