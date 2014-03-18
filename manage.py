3fbe3f3b00fef8839b46fc3e61dd0554873bed9a
Darek Walczak <walczak.darek@gmail.com>
2014-03-18 19:10:07 +0100

    local tests passed

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    sys.path.insert(0, '/Users/dwa/src/ZdecydujmyRazem/zdecydujmy-razem/ZDENV/lib/python2.7/site-packages/')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zdecydujmyrazem.settings")
     from django.core.management import execute_from_command_line
 
