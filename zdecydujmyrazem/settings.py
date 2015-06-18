edf9f0625a2924dddf241cfdfe4929615320d495
Lukasz Swierszcz <lukasz@koksownia.net>
2015-06-18 11:52:45 +0200

    Added django.contrib.gis to INSTALLED_APPS.

--- zdecydujmyrazem/settings.py
+++ zdecydujmyrazem/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 
     # 3rd party apps
     'notification',
