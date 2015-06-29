a5dc2673f63398e5950786b520f364a5271a69b2
Lukasz Swierszcz <lukasz@koksownia.net>
2015-06-29 13:53:14 +0200

    Maintenance mode with template.

--- zdecydujmyrazem/settings.py
+++ zdecydujmyrazem/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'maintenancemode.middleware.MaintenanceModeMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
